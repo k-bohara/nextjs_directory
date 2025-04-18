cb3720d37311d6d401ab1c58517626347659a02d
Kaman <kaman.bohara1010@gmail.com>
2025-04-18 19:30:53 +1000

    fixed minot bugs

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -15,7 +15,7 @@ import StartupCard, { StartupTypeCard } from '@/components/StartupCard'
 
 const md = markdownit()
 
-const Page = async ({ params }: { params: { id: string } }) => {
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id
   const [post, { select: editorPosts }] = await Promise.all([
     client.fetch(STARTUP_BY_ID_QUERY, { id }),
